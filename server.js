34ee2511292857575c9c8791925af0b3269ae9eb
Mladen <mladen.radenkovic@appmodule.net>
2023-02-03 21:19:01 +0100

    route_protect

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import jsonServer from 'json-server';
 import https from 'https';
 import fs from 'fs';
 import { config } from "./config.js"
-const { backHost, backPort } = config
+const { backHost, backPort, keyFile } = config
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
@@ -10,12 +10,23 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser)
+
+server.get('/korisnici', (req, res, next) => {
+  if (!Object.keys(req.query).length) {
+    return res.status(403).send()
+  }
+  next()
+})
+server.get('/odgovori', (req, res) => {
+  res.status(403).send()
+})
+
 server.use(router)
 
 https
   .createServer(
     {
-      key: fs.readFileSync('key.pem'),
+      key: fs.readFileSync(keyFile || 'privkey.pem'),
       cert: fs.readFileSync('cert.pem'),
     },
     server
